d57c72644fa8688fef2764115297ba256d03066e
19672005 <celikera001@gmail.com>
2025-01-02 14:34:08 +0300

    Create yazdırma.c

---
+++ yazdırma.c
@@ -0,0 +1,27 @@
+/* sayısal değerleri ekrana yazdırmak
+için printf fonksiyonunun kullanımı*/
+
+#include <stdio.h>
+
+int main(){
+    
+    int a=3;
+    int b=5;
+    int c=7;
+    
+    float k=0.15;
+    float m=1.25;
+    float x=3.8;
+    
+    
+    printf("k sayısı: %f\n", k);
+    printf("a sayısı: %d\n", a) ;       
+    printf("m sayısı: %f\n", m) ;
+    printf("b sayısı: %d\n", b);         
+    printf("x sayısı: %f\n", x);         
+    printf("c sayısı: %d", c);
+    
+    return 0;
+    
+    
+}
